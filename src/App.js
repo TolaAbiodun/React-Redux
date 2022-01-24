9d2764ec5e8ad39686033aacfad0c74bc83eff35
TolaAbiodun <adetolaabiodun@outlook.com>
2022-01-24 08:15:10 +0100

    final code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer, useRef } from "react";
+import Home from "./components/Home";
+import UseRefComponent from "./components/UseRef";
+import UselayoutEffect from "./components/UselayoutEffect";
+import ImperativeHandle from "./components/ImperativeHandle";
 
-function App() {
+
+// import { useSelector, useDispatch } from "react-redux";
+// import { increment, decrement } from "./actions";
+
+// function App() {
+//   const counter = useSelector(state => state.counter);
+//   const dispatch = useDispatch();
+
+//   const incrementHandler = () => dispatch(increment(5));
+//   const decrementHandler = () => dispatch(decrement());
+//   return (
+//     <div className="App">
+//       <div>
+//         <h1>Count: {counter}</h1>
+//         <button onClick={incrementHandler}>Increment</button>
+//         <button onClick={decrementHandler}>Decrement</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// UseReducer HOOK
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1, showText: state.showText };
+    case "DECREMENT":
+      return {
+        count: state.count === 0 ? 0 : state.count - 1,
+        showText: state.showText,
+      };
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
+
+  const incrementHandler = () => dispatch({ type: "INCREMENT" });
+  const decrementHandler = () => dispatch({ type: "DECREMENT" });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Count: {state.count}</h1>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <Home />
+        <UseRefComponent />
+        <UselayoutEffect />
+        <ImperativeHandle />
+
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,70 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import { createStore } from 'redux';
+// import allReducers from './reducers';
+
+// Import provider to connect global store to our App 
+// import {Provider} from 'react-redux';
+
+
+
+// Combine all reducers
+// const store = createStore(
+//   allReducers,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+
+// import { createStore } from 'redux';
+
+
+// // ACTION - Returns an object of actions (Increment and Decrement)
+
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
+
+
+// // REDUCER - Returns Switch case statements  of Actual Logic
+// const counter = (state=0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//     default: 
+//       return state;
+//   }
+// }
+
+// // STORE - GLOBALIZED VARIABLES
+
+// const store  = createStore(counter);
+
+// // Display in console
+// store.subscribe(() => console.log(store.getState()));
+
+
+// // DISPATCH - Execute actions
+// store.dispatch(increment());
+// store.dispatch(decrement());
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+      <App />
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
